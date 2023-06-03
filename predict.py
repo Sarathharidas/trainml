78c2e9e63704f49ea46cbcd7c6a2a9bb17c136fb
Sarathharidas <30758858+Sarathharidas@users.noreply.github.com>
2023-06-03 14:47:30 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -15,7 +15,7 @@ from fastapi.responses import FileResponse
 data_dir = os.environ.get("TRAINML_DATA_PATH")
 output_dir = os.environ.get("TRAINML_OUTPUT_PATH")
 
-
+@app.post("/predict")
 def predict_base64_voice(name, contents):
     wav_file = open("temp.wav", "wb")
     decode_string = base64.b64decode(contents)
@@ -27,16 +27,4 @@ def predict_base64_voice(name, contents):
     return {'size':size_file)
     #return FileResponse('subtitle.srt', media_type='application/octet-stream', filename='temp.srt')
         
-
-
-if __name__ == "__main__":
-    for filename in glob.glob(f"{data_dir}/*.JPEG"):
-        classes = predict_image(filename)
-        input_file = os.path.basename(filename)
-
-        output_file_name = re.sub(".JPEG", "_pred.json", input_file)
-
-        print(f"{output_file_name}: {classes}")
-
-        with open(f"{output_dir}/{output_file_name}", "w") as f:
-            json.dump(dict(file=input_file, classes=classes), f)
+   
