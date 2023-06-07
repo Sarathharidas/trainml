ecd69d9129cf75956cfb2a03c98c595999197898
Sarathharidas <30758858+Sarathharidas@users.noreply.github.com>
2023-06-07 11:41:40 -0400

    Update predict.py
    f name

--- predict.py
+++ predict.py
@@ -15,15 +15,16 @@ import aiofiles
 
 data_dir = os.environ.get("TRAINML_DATA_PATH")
 output_dir = os.environ.get("TRAINML_OUTPUT_PATH")
+model_dir = os.environ.get("TRAINML_MODEL_PATH")
 
 
 def predict_base64_voice(name, content):
-    wav_file = open("temp.wav", "wb")
+    wav_file = open(f"{model_dir}/temp.wav", "wb")
     decode_string = base64.b64decode(content)
     wav_file.write(decode_string)
-    os.system('whisper "temp.wav" --task translate --model large')
+    os.system(f'whisper "{model_dir}/temp.wav" --task translate --model large --output_dir {model_dir}')
     #FileResponse('temp.srt')
-    return FileResponse('file.srt', media_type='text/plain', filename='temp.srt')
+    return FileResponse(f'temp.srt', media_type='text/plain', filename=f'{model_dir}/temp.srt')
 
 
             
